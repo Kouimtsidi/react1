5c11f9c7f4ff1cd518fee37eecd5c7b20f91957a
Niki Kouimtsidi <kuiumcheva01@gmail.com>
2022-08-26 19:30:27 +0300

    add react.js

---
+++ react.js
@@ -0,0 +1,52 @@
+// const numbers = [-1, 5, 9, 17, -21, 23, -2];
+// const positiveNum = numbers.filter ((i)=> i >= 0 );
+// console.log(positiveNum)
+// const totalNum = positiveNum.reduce((total,currentValue)=>{
+//     return total + currentValue;
+// });
+// console.log(totalNum)
+
+
+const letter = ["a","b","b","a","b","c","b"];
+//   const newArray =arr.reduce(function(prev,cur){
+//     prev[cur] = (prev[cur]||0)+1;
+//     return prev;
+//   },{});
+
+????????????? 
+// const newArray = letter.reduce((total,currentLetter) => {
+// if (currentLetter = "a") {
+    // რა პირობით დავათვლევინო ა ელემენტები არ ვიცი
+// }
+// });
+// console.log(newArray);
+
+??????????
+// class Car {
+//     constructor(brand,model,speed,motion) {
+//         this.brand = brand;
+//         this.model = model;
+//         this.speed = speed;
+//         this.motion= motion;
+//     }
+// }
+// const speed = (x,y=0)=>x + y
+// console.log(speed(80))
+// const p1 = new Car ("Ford","Mustang","0");
+// console.log(p1)
+// ckeck_motion = () => {
+//     if (speed > 0){
+//     return this.motion = "მანქანა მოძრაობს";
+// }else {
+//     this.motion = "მანქანა გაჩერებულია";
+// }
+// }
+
+???????????????
+// const x = [ 10, 20];
+// const promise = new AddAsunc ((resolve,reject) =>{
+//     if( x = [ 10,20];{
+//         resolve ()
+//     })
+// })
+
