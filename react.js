f27290c88518b1777a31724b02064732a8f655bf
Niki Kouimtsidi <kuiumcheva01@gmail.com>
2022-08-27 11:48:17 +0300

    add homework

--- react.js
+++ react.js
@@ -1,52 +1,63 @@
-// const numbers = [-1, 5, 9, 17, -21, 23, -2];
-// const positiveNum = numbers.filter ((i)=> i >= 0 );
-// console.log(positiveNum)
-// const totalNum = positiveNum.reduce((total,currentValue)=>{
-//     return total + currentValue;
-// });
-// console.log(totalNum)
-
-
+//                                                                    პირველი
+const numbers = [-1, 5, 9, 17, -21, 23, -2];
+const positiveNum = numbers.filter ((i)=> i >= 0 );
+console.log(positiveNum)
+const totalNum = positiveNum.reduce((total,currentValue)=>{
+    return total + currentValue;
+});
+console.log(totalNum)
+//                                                                     მეორე
 const letter = ["a","b","b","a","b","c","b"];
-//   const newArray =arr.reduce(function(prev,cur){
-//     prev[cur] = (prev[cur]||0)+1;
-//     return prev;
-//   },{});
-
-????????????? 
-// const newArray = letter.reduce((total,currentLetter) => {
-// if (currentLetter = "a") {
-    // რა პირობით დავათვლევინო ა ელემენტები არ ვიცი
-// }
-// });
-// console.log(newArray);
-
-??????????
+  const newArray = letter.reduce(function(prev,cur){
+    prev[cur] = (prev[cur]||0)+1;
+    return prev;
+  },{});
+console.log(newArray);
+// მე-12 ხაზი ზეპირად ვიცი 
+//                                                                     მესამე
 // class Car {
 //     constructor(brand,model,speed,motion) {
 //         this.brand = brand;
 //         this.model = model;
-//         this.speed = speed;
-//         this.motion= motion;
-//     }
-// }
-// const speed = (x,y=0)=>x + y
-// console.log(speed(80))
-// const p1 = new Car ("Ford","Mustang","0");
-// console.log(p1)
-// ckeck_motion = () => {
-//     if (speed > 0){
-//     return this.motion = "მანქანა მოძრაობს";
-// }else {
-//     this.motion = "მანქანა გაჩერებულია";
-// }
-// }
+//         this.speed =
+//         this.motion= 
+// ?? აქ პირობა ვერ გავიგე 
 
-???????????????
-// const x = [ 10, 20];
-// const promise = new AddAsunc ((resolve,reject) =>{
-//     if( x = [ 10,20];{
-//         resolve ()
-//     })
+//                                                                     მეოთხე
+
+// const x = 5;
+// const y  = 10;
+// function addAsync (x,y){
+//     return new Promise ((resolve, reject)=>  {
+//         if ( x>3 && y > 4){
+//             resolve("hello");
+//         }else {
+//             reject("error");
+//     }
 // })
+
+// .then((resolve) =>
+//     console.log(resolve)
+// )
+// .catch((error)=>
+//     console.log(error));
+// }; ეს ჩანაწერი მგონი არასწორია
+
+const x = 5;
+const y = 11;
+const addAsync = new Promise ((resolve, reject) => {
+        if ( x>3 && y > 4){
+            resolve("hello");
+        }else {
+            reject("error");
+    }
+})
+
+.then((resolve) =>
+    console.log(resolve)
+)
+.catch((error)=>
+    console.log(error));
+
+   
 
